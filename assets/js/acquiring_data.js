da5636554445fea52ce1f7961bcc28b02b4aa33d
Ross <ross.e.macd@gmail.com>
2016-03-14 21:19:33 -0300

    Added files via upload

--- assets/js/acquiring_data.js
+++ assets/js/acquiring_data.js
@@ -42,33 +42,36 @@ For documentation, see http://underscorejs.org/
 // We'll set the table on this page to have a class of 'mytable'.
 // This way we can reference it in jQuery as $('.mytable') and not have to worry about duplicate data.
 $('table').addClass('mytable');
-
+//$('#mw-content-text').children('table').eq(3).addClass('mytable');
 
 // Get indexes; we will be using these a lot!
 // Discuss: _.map, .eq(0), obj + iter, .text()
-indexKeys = _.map($('.mytable tbody').children('tr').eq(0).children('th'), function(obj){
-  return $(obj).text()
+indexKeys = _.map($('.mytable').children('thead').find('th'), 
+            function(obj){
+    return $(obj).text().trim()
 })
+
 
 // Map a row:
 // Discuss: .trim(), .parseFloat(), .replace()
-var vals = [];
-vals = _.map($('.mytable tbody').children('tr').eq(1).children('td'), function(obj,iter){
-  var val = iter == 0 ? $(obj).text().trim() : parseFloat($(obj).text().replace(/\,/g, ''))
-  return val;
-})
+//var vals = [];
+//vals = _.map($('.mytable tbody').children('tr').eq(1).children('td'), function(obj,iter){
+//  var val = iter == 0 ? $(obj).text().trim() : parseFloat($(obj).text().replace(/\,/g, ''))
+//  return val;
+//})
 
 // Merge indexes with vals:
-_.object(indexKeys, vals);
+//_.object(indexKeys, vals);
 
 // Now iterate over the full table!
 // discuss: _.object(), map within a map, local variable scope
-var bigmap = _.map($('.mytable tbody').children('tr:not(:first):not(:last)'), function(obj,iter){
-  var vals = _.map($(obj).children('td'), function(obj2,iter2){
-    var value = iter2 == 0 ? $(obj2).text().trim() : parseFloat($(obj2).text().replace(/\,/g, ''))
-    return value;
-  }); //innermap
-  return _.object(indexKeys, vals);
+var bigmap = _.map($('.mytable tbody').children('tr'), function(obj,iter){
+    var vals = _.map($(obj).children(), function(obj2,iter2){
+        var value = iter2 == 0 ? $(obj2).text().trim() : parseFloat($(obj2).text().replace(/\,|r/g, ''))
+        //console.log(value)
+        return value;
+    }); //innermap
+    return _.object(indexKeys, vals);
 }); //outermap
 
 // Brief interlude here to talk about console.table
@@ -85,48 +88,48 @@ Let's look at some of the neat applications that Underscore allows us to leverag
 
 //// Min, Max, Sorting
 // Biggest value in 2013:
-_.max(bigmap, function(obj,iter){
-  return obj['Value 2013'];
-})
-
-// Biggest jump from 2012 to 2013:
-_.max(bigmap, function(obj,iter){
-  return obj['Value 2013'] / obj['Value 2012'];
-})
-
-// Sort by percentage of 2012 import to 2013:
-_.sortBy(bigmap, function(obj,iter){
-  return obj['Value 2013'] / obj['Value 2012'];
-})
+//_.max(bigmap, function(obj,iter){
+//  return obj['Value 2013'];
+//})
+//
+//// Biggest jump from 2012 to 2013:
+//_.max(bigmap, function(obj,iter){
+//  return obj['Value 2013'] / obj['Value 2012'];
+//})
+//
+//// Sort by percentage of 2012 import to 2013:
+//_.sortBy(bigmap, function(obj,iter){
+//  return obj['Value 2013'] / obj['Value 2012'];
+//})
 
 
 
 // Filtering and reducing
 // Filter by values greater than or less than
 // Discuss: _.filter, _.find, etc.
-_.filter(bigmap, function(obj,iter){
-  return obj['Value 2013'] < 50;
-})
-
-// Find those rows that have "Agriculture" in their title:
-_.filter(bigmap, function(obj,iter){
-  return obj['End-Use Code'].indexOf('Agric') > 0;
-})
-
-// Find those rows in the 40,000 category: Household Goods
-// Discuss: why we're using > -1
-_.filter(bigmap, function(obj,iter){
-  return obj['End-Use Code'].indexOf('(4') > -1;
-})
-
-// Total price of all household exports in 2013:
-// Discuss: _.reduce()
-var household = _.filter(bigmap, function(obj,iter){
-  return obj['End-Use Code'].indexOf('(4') > -1;
-})
-_.reduce(_.pluck(household, 'Value 2013'), function(memo,num){
-  return memo+num;
-})
+//_.filter(bigmap, function(obj,iter){
+//  return obj['Value 2013'] < 50;
+//})
+//
+//// Find those rows that have "Agriculture" in their title:
+//_.filter(bigmap, function(obj,iter){
+//  return obj['End-Use Code'].indexOf('Agric') > 0;
+//})
+//
+//// Find those rows in the 40,000 category: Household Goods
+//// Discuss: why we're using > -1
+//_.filter(bigmap, function(obj,iter){
+//  return obj['End-Use Code'].indexOf('(4') > -1;
+//})
+//
+//// Total price of all household exports in 2013:
+//// Discuss: _.reduce()
+//var household = _.filter(bigmap, function(obj,iter){
+//  return obj['End-Use Code'].indexOf('(4') > -1;
+//})
+//_.reduce(_.pluck(household, 'Value 2013'), function(memo,num){
+//  return memo+num;
+//})
 
 
 
@@ -143,37 +146,43 @@ Each one corresponds with a common starting number in its End-Use Code.
 =======================================================*/
 
 // Let's index all of our categories:
-var food = _.filter(bigmap, function(obj,iter){
-  return obj['End-Use Code'].indexOf('(0') > -1;
+var atlanticCanada = _.filter(bigmap, function(obj,iter){
+    if(obj['Geography']){
+        return obj['Geography'].indexOf('[1') > -1;
+    }
 })
-var chem = _.filter(bigmap, function(obj,iter){
-  return obj['End-Use Code'].indexOf('(1') > -1;
+
+var quebec = _.filter(bigmap, function(obj,iter){
+    if(obj['Geography']){
+        return obj['Geography'].indexOf('[2') > -1;
+    }
 })
-var industrial = _.filter(bigmap, function(obj,iter){
-  return obj['End-Use Code'].indexOf('(2') > -1;
+
+var ontario = _.filter(bigmap, function(obj,iter){
+    if(obj['Geography']){
+        return obj['Geography'].indexOf('[3') > -1;
+    }
 })
-var transportation = _.filter(bigmap, function(obj,iter){
-  return obj['End-Use Code'].indexOf('(3') > -1;
+
+var praries = _.filter(bigmap, function(obj,iter){
+    if(obj['Geography']){
+        return obj['Geography'].indexOf('[4') > -1;
+    }
 })
-var household = _.filter(bigmap, function(obj,iter){
-  return obj['End-Use Code'].indexOf('(4') > -1;
-})
-var military = _.filter(bigmap, function(obj,iter){
-  return obj['End-Use Code'].indexOf('(5') > -1;
-})
-var misc = _.filter(bigmap, function(obj,iter){
-  return obj['End-Use Code'].indexOf('(6') > -1;
+
+var bc = _.filter(bigmap, function(obj,iter){
+    if(obj['Geography']){
+        return obj['Geography'].indexOf('[5') > -1;
+    }
 })
 
 // And include them in a large array of objects:
 var hugemap = [
-  {"name": "food", "values": food},
-  {"name": "chem", "values": chem},
-  {"name": "industrial", "values": industrial},
-  {"name": "transportation", "values": transportation},
-  {"name": "household", "values": household},
-  {"name": "military", "values": military},
-  {"name": "misc", "values": misc}
+  {"name": "atlanticCanada", "values": atlanticCanada},
+  {"name": "quebec", "values": quebec},
+  {"name": "ontario", "values": ontario},
+  {"name": "praries", "values": praries},
+  {"name": "bc", "values": bc}
 ]
 
 
@@ -271,7 +280,11 @@ STEP 5: EXPORT THE MASTER MAP IN TEXT FORMAT
 // this will give us the array exploded out into a string; let's copy it (everything but the surrounding quotes) so we can use it in our next excercise.
 JSON.stringify(comparable);
 
-var exportsToCanada = [{"id":"Value 2004","stats":[{"name":"food","value":10932929},{"name":"chem","value":47670039},{"name":"industrial","value":53236497},{"name":"transportation","value":50017241},{"name":"household","value":23395551},{"name":"military","value":453961},{"name":"misc","value":4173651}]},{"id":"Value 2005","stats":[{"name":"food","value":12045684},{"name":"chem","value":55398959},{"name":"industrial","value":60654420},{"name":"transportation","value":53606698},{"name":"household","value":25754653},{"name":"military","value":426801},{"name":"misc","value":4011470}]},{"id":"Value 2006","stats":[{"name":"food","value":13597489},{"name":"chem","value":61251666},{"name":"industrial","value":64778364},{"name":"transportation","value":56772879},{"name":"household","value":29330511},{"name":"military","value":408613},{"name":"misc","value":4516491}]},{"id":"Value 2007","stats":[{"name":"food","value":15828932},{"name":"chem","value":66940063},{"name":"industrial","value":66654562},{"name":"transportation","value":60112470},{"name":"household","value":33238849},{"name":"military","value":967493},{"name":"misc","value":5145773}]},{"id":"Value 2008","stats":[{"name":"food","value":18219404},{"name":"chem","value":78250553},{"name":"industrial","value":70007956},{"name":"transportation","value":53305854},{"name":"household","value":35125349},{"name":"military","value":973378},{"name":"misc","value":5267337}]},{"id":"Value 2009","stats":[{"name":"food","value":17824759},{"name":"chem","value":56023688},{"name":"industrial","value":56578386},{"name":"transportation","value":36755371},{"name":"household","value":32537736},{"name":"military","value":577091},{"name":"misc","value":4360923}]},{"id":"Value 2010","stats":[{"name":"food","value":19348514},{"name":"chem","value":71248282},{"name":"industrial","value":67126144},{"name":"transportation","value":49544427},{"name":"household","value":36118327},{"name":"military","value":875765},{"name":"misc","value":4995005}]},{"id":"Value 2011","stats":[{"name":"food","value":21855377},{"name":"chem","value":84782247},{"name":"industrial","value":76557266},{"name":"transportation","value":54471786},{"name":"household","value":37069568},{"name":"military","value":940553},{"name":"misc","value":5614733}]},{"id":"Value 2012","stats":[{"name":"food","value":23311658},{"name":"chem","value":85455718},{"name":"industrial","value":80737858},{"name":"transportation","value":59132702},{"name":"household","value":37528846},{"name":"military","value":536554},{"name":"misc","value":5947207}]},{"id":"Value 2013","stats":[{"name":"food","value":24219696},{"name":"chem","value":91480126},{"name":"industrial","value":79593366},{"name":"transportation","value":60460811},{"name":"household","value":38014667},{"name":"military","value":588784},{"name":"misc","value":7252143}]}];
+JSON.stringify(comparable, function(key, val) {
+    return val.toFixed ? Number(val.toFixed(2)) : val;
+})
+
+//var exportsToCanada = [{"id":"Value 2004","stats":[{"name":"food","value":10932929},{"name":"chem","value":47670039},{"name":"industrial","value":53236497},{"name":"transportation","value":50017241},{"name":"household","value":23395551},{"name":"military","value":453961},{"name":"misc","value":4173651}]},{"id":"Value 2005","stats":[{"name":"food","value":12045684},{"name":"chem","value":55398959},{"name":"industrial","value":60654420},{"name":"transportation","value":53606698},{"name":"household","value":25754653},{"name":"military","value":426801},{"name":"misc","value":4011470}]},{"id":"Value 2006","stats":[{"name":"food","value":13597489},{"name":"chem","value":61251666},{"name":"industrial","value":64778364},{"name":"transportation","value":56772879},{"name":"household","value":29330511},{"name":"military","value":408613},{"name":"misc","value":4516491}]},{"id":"Value 2007","stats":[{"name":"food","value":15828932},{"name":"chem","value":66940063},{"name":"industrial","value":66654562},{"name":"transportation","value":60112470},{"name":"household","value":33238849},{"name":"military","value":967493},{"name":"misc","value":5145773}]},{"id":"Value 2008","stats":[{"name":"food","value":18219404},{"name":"chem","value":78250553},{"name":"industrial","value":70007956},{"name":"transportation","value":53305854},{"name":"household","value":35125349},{"name":"military","value":973378},{"name":"misc","value":5267337}]},{"id":"Value 2009","stats":[{"name":"food","value":17824759},{"name":"chem","value":56023688},{"name":"industrial","value":56578386},{"name":"transportation","value":36755371},{"name":"household","value":32537736},{"name":"military","value":577091},{"name":"misc","value":4360923}]},{"id":"Value 2010","stats":[{"name":"food","value":19348514},{"name":"chem","value":71248282},{"name":"industrial","value":67126144},{"name":"transportation","value":49544427},{"name":"household","value":36118327},{"name":"military","value":875765},{"name":"misc","value":4995005}]},{"id":"Value 2011","stats":[{"name":"food","value":21855377},{"name":"chem","value":84782247},{"name":"industrial","value":76557266},{"name":"transportation","value":54471786},{"name":"household","value":37069568},{"name":"military","value":940553},{"name":"misc","value":5614733}]},{"id":"Value 2012","stats":[{"name":"food","value":23311658},{"name":"chem","value":85455718},{"name":"industrial","value":80737858},{"name":"transportation","value":59132702},{"name":"household","value":37528846},{"name":"military","value":536554},{"name":"misc","value":5947207}]},{"id":"Value 2013","stats":[{"name":"food","value":24219696},{"name":"chem","value":91480126},{"name":"industrial","value":79593366},{"name":"transportation","value":60460811},{"name":"household","value":38014667},{"name":"military","value":588784},{"name":"misc","value":7252143}]}];
 
 // And now we're off to the races!
 
